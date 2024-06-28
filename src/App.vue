6bd86aff3c28e2bfe524c82405a764bd97dac67a
lenin-developer <40728531+lenin-developer@users.noreply.github.com>
2024-06-28 13:27:11 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ColorComponent from './components/ColorComponent.vue';
+</script>
+
+<template>
+  <ColorComponent />
+</template>
+
+<style scoped></style>
---
+++ src/components/ColorComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+const colorFondo = ref('#000000'); // Color default value
+const colortext = ref('#ffffff'); // Color default value
+</script>
+<template>
+  <div class="wrapper">
+    <div class="cube">
+      <p>Color</p>
+    </div>
+    <p>
+    color de fondo:
+    <input type="color" v-model="colorFondo">
+    </p>
+
+    <p>
+    color de texto:
+    <input type="color" v-model="colortext">
+    </p>
+    
+  
+  </input>
+   
+  </div>
+</template>
+
+<style scoped>
+.cube {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+  height: 80px;
+  width: 80px;
+  background: v-bind(colorFondo);
+  color: v-bind(colortext);
+  font-weight: 900;
+}
+</style>
